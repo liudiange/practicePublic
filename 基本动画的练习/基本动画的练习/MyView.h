48e254d9f17634bf14a8025daf11143fb8b6a864
liudiange <shaoyeliudiange@163.com>
2018-03-19 18:39:02 +0800

    自己的view

---
+++ 基本动画的练习/基本动画的练习/MyView.h
@@ -0,0 +1,13 @@
+//
+//  MyView.h
+//  基本动画的练习
+//
+//  Created by 刘殿阁 on 2018/3/18.
+//  Copyright © 2018年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyView : UIView
+
+@end
---
+++ 基本动画的练习/基本动画的练习/MyView.m
@@ -0,0 +1,21 @@
+//
+//  MyView.m
+//  基本动画的练习
+//
+//  Created by 刘殿阁 on 2018/3/18.
+//  Copyright © 2018年 apple. All rights reserved.
+//
+
+#import "MyView.h"
+
+@implementation MyView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
