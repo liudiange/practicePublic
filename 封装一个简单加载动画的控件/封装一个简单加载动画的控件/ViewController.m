cc7b293ef797f391d55b9ef91d40f5abdd8bcca3
shaoyeliudiange <shaoyeliudiange@163.com>
2018-09-07 17:55:32 +0800

    三个控件的修改部分的代码

--- 封装一个简单加载动画的控件/封装一个简单加载动画的控件/ViewController.m
+++ 封装一个简单加载动画的控件/封装一个简单加载动画的控件/ViewController.m
@@ -72,7 +72,7 @@
 }
 -(void)addDefaultView{
     
-    [self.view DG_addEmptyViewWithType:DGEmptyViewTypeNoData withDefaultY:0 checkBlock:^BOOL{
+    [self.view DG_addEmptyViewWithType:DGEmptyViewTypeNoData withDefaultY:200 checkBlock:^BOOL{
         
         return self.dataArray.count;
         
